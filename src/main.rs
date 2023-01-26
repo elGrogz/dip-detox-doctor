8c68ec4ed980f990ad03201cb9b508be76fee10d
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-01-26 13:02:10 +0100

    got initial node check running

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,43 @@
-use dip::prelude::*;
+use dip::{bevy::log::LogPlugin, prelude::*};
+// use std::path::Path;
+use cmd_lib::run_fun;
 
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugin(CliPlugin::<NoAsyncAction>::oneshot())
+    .add_plugin(LogPlugin)
+    .add_plugin(ActionPlugin)
+    .add_system(handle_check)
+    .run();
+}
+
+#[derive(CliPlugin, clap::Parser)]
+#[clap(author, version, about="Detox tool", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    action: Action,
+}
+
+#[derive(SubcommandPlugin, clap::Subcommand, Clone, Debug)] // attribute macro
+pub enum Action {
+    #[clap(about = "Checks your React Native and Detox installation")]
+    Check
+}
+
+fn handle_check(mut actions: EventReader<CheckAction>) {
+    actions.iter().for_each(|_action| {
+        println!("Checking!");
+
+        match run_fun!(which node) {
+            Ok(path) => {
+                println!("Node installs at: {path}");
+
+                match run_fun!(node -v) {
+                    Ok(version) => { println!("Node version: {version}");}
+                    Err(e) => {println!("Couldn't find Node version: {e}");}
+                }
+            }      
+            Err(e) => {println!("Error: {e}")}
+        }
+    });
 }
